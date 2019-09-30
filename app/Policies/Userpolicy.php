f4df40e7f2d18a92c528e21ccc465b6bc76fa8b7
rui <1975261197@qq.com>
2019-09-30 09:32:08 +0000

    访问策略

---
+++ app/Policies/Userpolicy.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class Userpolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function update(User $currentUser, User $user){
+            return $currentUser->id === $user->id;
+    }
+}
