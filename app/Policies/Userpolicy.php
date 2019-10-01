b596f1fde9c596b3dee0438968e00ff6fa4d4515
rui <1975261197@qq.com>
2019-10-01 07:00:10 +0000

    管理员可删除用户

--- app/Policies/Userpolicy.php
+++ app/Policies/Userpolicy.php
@@ -17,4 +17,8 @@ class Userpolicy
     public function update(User $currentUser, User $user){
             return $currentUser->id === $user->id;
     }
+    public function destroy(User $currentUser,User $user){
+            return $currentUser->is_admin && $currentUser->id !== $user->id;
+
+    }
 }
