6a93cd58361d040aec899f1355539c2b6d14e5a6
rui <1975261197@qq.com>
2019-10-02 06:39:36 +0000

    用户激活

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Auth;
+use Mail;
 
 class UsersController extends Controller
 {
@@ -12,29 +13,35 @@ class UsersController extends Controller
     public function create(){
         return view('users.create');
     }
+
     public function show(User $user){
         return view('users.show',compact('user'));
     }
-    public function store(Request $request){
-        $this->validate($request,[
-           'name' => 'required|max:50',
-           'email' => 'required|email|unique:users|max:255',
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
 
         $user = User::create([
-           'name' => $request->name,
-           'email' => $request->email,
-           'password' =>bcrypt($request->password),
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
         ]);
-        Auth::login($user);
-        session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show',[$user]);
+
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
+        return redirect('/');
     }
+
     public function edit(User $user){
         $this->authorize('update', $user);
         return view('users.edit',compact('user'));
     }
+
     public function update(User $user, Request $request)
     {
         $this->authorize('update', $user);
@@ -54,10 +61,11 @@ class UsersController extends Controller
 
         return redirect()->route('users.show', $user);
     }
+
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store','index']
+            'except' => ['show', 'create', 'store','index','confirmEmail']
         ]);
 
         $this->middleware('guest', [
@@ -69,10 +77,38 @@ class UsersController extends Controller
         $users = User::paginate(6);
         return view('users.index',compact('users'));
     }
+
     public function destroy(User $user){
         $this->authorize('destroy',$user);
         $user->delete();
         session()->flash('success','成功删除用户');
         return back();
+    }
+
+    public function confirmEmail($token)
+    {
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '恭喜你，激活成功！');
+        return redirect()->route('users.show', [$user]);
+    }
+
+    protected function sendEmailConfirmationTo($user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'summer@example.com';
+        $name = 'Summer';
+        $to = $user->email;
+        $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
+
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
     }
 }
